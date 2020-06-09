44972642588e949d4ba3008ef3f3ea4a4fb51891
nikola631 <nikolastankovic631@gmail.com>
2020-06-09 21:30:53 +0200

    Added: User auth, User save with encripted password and jwt

--- routs/api/users.js
+++ routs/api/users.js
@@ -1,9 +1,80 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const { check, validationResult } = require('express-validator');
+const bcript = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
-// @route  GET api/users
-// @desc   Test route
+
+const User = require('../../models/User')
+
+// @route  Post api/users
+// @desc   Register user
 // @acces  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', [
+    check('name', 'Name is requierd').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+        //See if user exists
+        let user = await User.findOne({ email });
+
+        if (user) {
+            res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        }
+
+        //Get users Gravatar
+
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        });
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+
+        //Encrypt password
+
+        let salt = await bcript.genSalt(10);
+
+        user.password = await bcript.hash(password, salt);
+
+        await user.save()
+
+        //Return jsonwebtoken
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtToken'), { expiresIn: 360000 }, (err, token) => {
+            if (err) {
+                throw err;
+            }
+            res.json(token);
+        });
+
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send('Server error');
+    }
+
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 //Connect to database
 ConnectDB();
 
+//Initialize Midleware
+app.use(express.json({ extended: false }))
+
 app.get('/', (req, res) => res.send('API Running'));
 
 //Define routes
