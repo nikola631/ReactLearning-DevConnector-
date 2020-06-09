57a522babcca863bbec66f21612d5847a34d18f9
nikola631 <nikolastankovic631@gmail.com>
2020-06-09 11:38:33 +0200

    Routs added

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ ConnectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+//Define routes
+app.use('/api/users', require('./routs/api/users'));
+app.use('/api/profile', require('./routs/api/profile'));
+app.use('/api/auth', require('./routs/api/auth'));
+app.use('/api/posts', require('./routs/api/posts'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
